e70875b718e54662c4aafb31697a315fd370c5ee
HildeNancy25 <hildenancyisingizwe@gmail.com>
2024-03-12 11:46:50 +0200

    commit emoji

---
+++ index.js
@@ -0,0 +1,17 @@
+const emojiFace = document.getElementById("emoji");
+// const emojiFace = document.querySelector("#emoji");
+const generate = document.getElementById("btn");
+const emo = ["😘", "🌭", "😒", "🥗", "😉", "🍔", "😍" ,"🌮"];
+
+emojiFace.innerHTML = 'wow!';
+
+const displayEmo = () => {
+    var a = Math.floor(Math.random() * emo.length);   
+    let display = emo[a];
+    emojiFace.innerHTML = display;
+}
+
+// generate.addEventListener("click", displayEmo);
+generate.addEventListener("click" , displayEmo);
+
+
